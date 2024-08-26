d72f8b2eb89683886dbf6cfb68c20efb59832564
Ryan <ryanswhiteman@gmail.com>
2024-08-26 13:09:22 +1000

    test for Ryan

--- src/main/java/edu/qut/cab302/wehab/HelloController.java
+++ src/main/java/edu/qut/cab302/wehab/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to We-Hab! Or is it WeHab? Or We Hab?");
     }
 }
