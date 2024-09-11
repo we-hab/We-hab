6382a5ba33cce0e589952b1b6099e76c2558895d
Ryan <ryanswhiteman@gmail.com>
2024-09-11 19:29:39 +1000

    Update

--- src/main/java/edu/qut/cab302/wehab/MainApplication.java
+++ src/main/java/edu/qut/cab302/wehab/MainApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("create-account.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
