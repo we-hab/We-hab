fbe059491b395cbb0966b7357a2c27f75b89bd47
Matt <mattpatchava@hotmail.com>
2024-09-20 12:50:16 +1000

    Added maven-surefire-plugin for troubleshooting JUnit issues

--- src/test/java/edu/qut/cab302/wehab/medication/FDAApiServiceTest.java
+++ src/test/java/edu/qut/cab302/wehab/medication/FDAApiServiceTest.java
@@ -2,12 +2,12 @@ package edu.qut.cab302.wehab.medication;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
 import org.mockito.Mock;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import org.skyscreamer.jsonassert.JSONAssert;
