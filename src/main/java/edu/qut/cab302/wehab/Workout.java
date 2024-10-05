3d1c13dab86b988fced22793205b3bec21fc82b2
alexmccccc <67624118+alexmccccc@users.noreply.github.com>
2024-10-05 14:06:04 +1000

    Rearranged Workout class and included additional comments.

--- src/main/java/edu/qut/cab302/wehab/Workout.java
+++ src/main/java/edu/qut/cab302/wehab/Workout.java
@@ -1,52 +1,60 @@
 package edu.qut.cab302.wehab;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Workout {
     private String workoutType;
     private LocalDate date;
-    private int duration; // in minutes
-    private int effort; // scale 1-5
+    private int duration;
+    private int effort;
 
-    // Constructor
+    /** @return The type of workout. */
+    public String getWorkoutType() {
+        return workoutType;
+    }
+
+    /** @return The date. */
+    public LocalDate getDate() {
+        return date;
+    }
+
+    /** @return The duration of the workout. */
+    public int getDuration() {
+        return duration;
+    }
+
+    /** @return The effort of the workout. */
+    public int getEffort() {
+        return effort;
+    }
+
+    /** @param workoutType Sets the type of workout*/
+    public void setWorkoutType(String workoutType) {
+        this.workoutType = workoutType;
+    }
+
+    /** @param date Sets the date of workout*/
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    /** @param duration Sets the duration of workout*/
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    /** @param effort Sets the effort of workout*/
+    public void setEffort(int effort) {
+        this.effort = effort;
+    }
+
+    /**
+     * Constructs a Workout object by parsing user input from the GUI.
+     */
     public Workout(String workoutType, LocalDate date, int duration, int effort) {
         this.workoutType = workoutType;
         this.date = date;
         this.duration = duration;
         this.effort = effort;
     }
-
-    // Getters and Setters
-    public String getWorkoutType() {
-        return workoutType;
-    }
-
-    public void setWorkoutType(String workoutType) {
-        this.workoutType = workoutType;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public int getEffort() {
-        return effort;
-    }
-
-    public void setEffort(int effort) {
-        this.effort = effort;
-    }
 }
-
