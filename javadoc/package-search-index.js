31d4a127000064b79f5bb7d49f0897d877e9c7d3
alexmccccc <alexmccccc@users.noreply.github.com>
2024-10-05 08:24:49 +0000

    Deploying to gh-pages from @ we-hab/We-hab@1331eaa416a636398ac84c0205be96047fa3bd09 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"edu.qut.cab302.wehab","l":"edu.qut.cab302.wehab"},{"m":"edu.qut.cab302.wehab","l":"edu.qut.cab302.wehab.medication"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"edu.qut.cab302.wehab","l":"edu.qut.cab302.wehab"},{"m":"edu.qut.cab302.wehab","l":"edu.qut.cab302.wehab.medication"},{"m":"edu.qut.cab302.wehab","l":"edu.qut.cab302.wehab.workout"}];updateSearchResults();
